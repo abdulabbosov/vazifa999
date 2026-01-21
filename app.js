1a91bd48ff8e62236f8e61f6b6911d2f73cccda8
shahriyor <abdulabbosov09@gmail.com>
2026-01-21 21:41:49 +0500

    9000

--- app.js
+++ app.js
@@ -1,69 +1,9 @@
+// AOS init
+AOS.init({duration:1000, once:true});
 
-import { elgameZone, elresultZone, elHands, elRobot, elUser, elrefreshGameButton  } from "./html-elements.js";
-
-function robotChoose() {
-const hands = ["rock", "paper", "scissors"];
-const randomIndex = Math.trunc( Math.random() * hands.length ); 
-return hands [randomIndex];
-
-}
-robotChoose();
-
-function swapZone(boolean) {
-    if(boolean) {
-        elgameZone.style.display = "none";
-        elresultZone.style.display = "flex";
-
-    } else {
-         elgameZone.style.display = "flex";
-        elresultZone.style.display = "none";
-    }
-}
-
-function chekWinner (user, robot) {
-    if(user === robot) {
-        return "TIE";       
-    }
-    else if (user ==="paper" && robot === "scissors") {
-        return "ROBOT";
-      }   
-        else if (user ==="scissors" && robot === "rock") {
-        return "ROBOT";
-      }   
-       else if (user ==="rock" && robot === "paper") {
-        return "ROBOT";
-      }   else {
-        return "USER";
-      }
-
-}
-
-elHands.forEach((elHand) => {
-    elHand.addEventListener("click", (evt)=> {
-
-          swapZone(true);
-
-       
-        const user = evt.target.alt;
-        
-       
-        const robot = robotChoose();
-
-    elUser.src = evt.target.src;
-    elRobot.src = "./img/hand-load.svg";
-    
-    setTimeout(() => {
-        elRobot.src = `./img/${robot}.svg`;
-
-        const winner = chekWinner(user, robot);
-        console.log(winner);
-         
-        
-    }, 1000);
-        
-    });
-}); 
-
-elrefreshGameButton.addEventListener("click", ()=> {
-    swapZone(false);
-} );
+// Navbar toggle
+const toggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+toggle.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
